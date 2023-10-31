74820eb5f8ca454efd8ebddc7c3b57378fd8115e
siqi <v_lsiqiliu@tencent.com>
2023-10-31 14:25:54 +0800

    版本调整/ version update

--- renren-fast/src/main/java/io/ren/RenrenApplication.java
+++ renren-fast/src/main/java/io/ren/RenrenApplication.java
@@ -8,7 +8,7 @@
 
 package io.ren;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- renren-fast/src/main/java/io/ren/config/MybatisPlusConfig.java
+++ renren-fast/src/main/java/io/ren/config/MybatisPlusConfig.java
@@ -8,6 +8,7 @@
 
 package io.ren.config;
 
+
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
