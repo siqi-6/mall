3c236d8c16caf527b1b73caec50c248c4c710a39
siqi <v_lsiqiliu@tencent.com>
2023-11-01 17:25:28 +0800

    网关上nacos

--- member/src/test/java/com/example/member/MemberApplicationTests.java
+++ member/src/test/java/com/example/member/MemberApplicationTests.java
@@ -1,13 +1,11 @@
 package com.example.member;
 
-import com.example.member.service.pub.FeignService;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MemberApplicationTests {
-
     @Test
     void contextLoads() {
     }
